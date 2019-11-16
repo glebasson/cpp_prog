27f71ca284db9d9a6644bf9342e793b5f3da2cca
glebasson <glebasson@yandex.ru>
2019-11-16 04:25:58 +0300

    vector remake

--- src/myvector/myvector.cpp
+++ src/myvector/myvector.cpp
@@ -4,21 +4,21 @@
 #include <fstream>
 #include <random>
 #include <algorithm>
+#include <iostream>
 
 // move init
 template <typename T>
 myvector<T>::myvector(myvector &&x) : 
-  data(x.data), len(x.len), capacity(x.capacity) {
+  data(x.data), len(x.len) {
   x.data = nullptr;
 }
 
 // copy init
 template <typename T>
 myvector<T>::myvector(const myvector &x) {
-  data = new T[x.capacity];
-  memcpy(data, x.data, sizeof(T) * x.len);
+  data = new T[x.len];
+  for(int i=0; i<x.len; i++) data[i] = x.data[i];
   len = x.len;
-  capacity = x.capacity;
 }
 
 template <typename T>
@@ -26,11 +26,10 @@ myvector<T>::~myvector() {
   delete[] data;
 }
 
-// init empty with fixed size
+// init empty 
 template <typename T>
-myvector<T>::myvector(int maxsize) {
-  data = new T[maxsize];
-  capacity = maxsize;
+myvector<T>::myvector() {
+  data = nullptr;
 }
 
 template <typename T>
@@ -40,7 +39,7 @@ T* myvector<T>::begin() {
 
 template <typename T>
 T* myvector<T>::end() {
-  return(data + len - 1);
+  return(data + len);
 }
 
 // move operator
@@ -48,12 +47,12 @@ template <typename T>
 myvector<T>& myvector<T>::operator=(myvector &&x) {
   if (&x == this)
     return this;
-  return(myvector(x));
+  return(myvector(std::move(x)));
 }
 
 // copy operator
 template <typename T>
-myvector<T>& myvector<T>::operator=(const myvector& x) {
+myvector<T>& myvector<T>::operator=(const myvector &x) {
   if (&x == this)
     return this;
   return(myvector(x));
@@ -69,6 +68,10 @@ void myvector<T>::swap(T &a, T &b) {
 // move push
 template <typename T>
 void myvector<T>::push_back(T&& value) {
+  T* td = new T[len + 1];
+  for(int i=0; i<len; i++) td[i] = std::move(data[i]);
+  delete[] data;
+  data = td;
   *(data + len) = std::move(value);
   len++;
 }
@@ -76,14 +79,18 @@ void myvector<T>::push_back(T&& value) {
 // copy push
 template <typename T>
 void myvector<T>::push_back(const T &value) {
+  T* td = new T[len + 1];
+  for(int i=0; i<len; i++) td[i] = data[i];
+  delete[] data;
+  data = td;
   *(data + len) = value;
   len++;
 }
 
 template <typename T>
-T& myvector<T>::pop_back() {
+void myvector<T>::pop_back() {
   len--;
-  return *(data + len);
+  delete (data + len);
 }
 
 template <typename T>
@@ -95,18 +102,36 @@ int myvector<T>::size() {
 
 
 int main() {
-  myvector<std::ofstream> v(4);
+  // myvector<int> v;
+  // v.push_back(10);
+  // v.push_back(20);
+  // myvector<int> d = v;
+  // d.push_back(50);
+  // d.push_back(70);
+  // d.push_back(80);
+
+  // d.swap(v);
+
+  // myvector<int> d(v);
+  // myvector<int> c = std::move(d);
+  // std::cout << d.data[2] << std::endl;
+  myvector<std::ofstream> v;
   v.push_back(std::ofstream("a"));
   v.push_back(std::ofstream("b"));
   v.push_back(std::ofstream("c"));
   v.push_back(std::ofstream("d"));
 
+  myvector<std::ofstream> d;
+  d.push_back(std::ofstream("q"));
+  d.push_back(std::ofstream("w"));
+  d.push_back(std::ofstream("r"));
+  d.push_back(std::ofstream("t"));
+
   std::random_device rd;
     std::mt19937 g(rd());
-    std::shuffle(v.begin(), v.end(), g);
+    std::shuffle(d.begin(), d.end(), g);
 
-    while(v.size())
-      v.pop_back() << v.size(); 
+  for(int i=0; i<v.size(); i++) d.data[i] << i;
 
   return(0);
 }
--- src/myvector/myvector.h
+++ src/myvector/myvector.h
@@ -1,11 +1,13 @@
 #ifndef MYmyvector
 #define MYmyvector
 
+#include <bits/stdc++.h>
+
 template <typename T>
 class myvector {
 public:
-	// fixed size init
-	myvector(int maxsize);
+	// empty init
+	myvector();
 
 	// move init
 	myvector(myvector &&x);
@@ -33,13 +35,30 @@ public:
 	// copy
 	void push_back(const T &value);
 
-	T& pop_back();
+	void pop_back();
 	int size();
 
-private:
+	void swap(myvector &x) {
+		T* tmp = new T[len];
+		for(int i=0; i<len; i++) data[i] = tmp[i];
+		delete[] data;
+		data = new T[x.len];
+		for(int i=0; i<x.len; i++) data[i] = x.data[i];
+		delete[] x.data;
+		x.data = new T[len];
+		for(int i=0; i<len; i++) x.data[i] = tmp[i];
+		int tmpl = len;
+		len = x.len;
+		x.len = tmpl;
+	}
+
+	void erase(int pos) {
+		for(int i=0; i<len; i++) delete data[i];
+		len = pos + 1;
+	}
+
 	T* data;
 	int len = 0;
-	int capacity;
 };
 
 #endif
