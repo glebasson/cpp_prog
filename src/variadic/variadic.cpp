27d3821df928ff81a2819cca1529f407cc38eae3
glebasson <glebasson@yandex.ru>
2019-11-16 09:49:17 +0300

    cat implementation

--- src/variadic/variadic.cpp
+++ src/variadic/variadic.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
-#include <bits/stdc++.h> 
+#include <bits/stdc++.h>
+#include <cstring> //memcpy
 
 
 void message(std::ostream& stream, std::string&& pattern){
@@ -26,3 +27,47 @@ void message(std::ostream& stream, std::string&& pattern,
 	stream << std::forward<head>(value);
 	message(stream, std::forward<std::string>(std::string( it + 1, pattern.end() )), std::forward<tail>(args)...);
 }
+
+
+// cpy
+template<int N, int S, typename AT, typename head>
+void cpy(AT &arr, head first){
+	for(int i=0; i<S; i++) arr[N*S + i] = first[i];
+}
+
+template<int N, int S, typename AT, typename head, typename... Args>
+void cpy(AT &arr, head first, Args... tail){
+	for(int i=0; i<S; i++) arr[N*S + i] = first[i];
+	cpy<N+1, S, AT, Args...>(arr, tail...);
+}
+
+
+// concat
+template<typename head, typename... Args>
+struct concantinator {
+	typedef typename head::value_type arraytype;
+	static const size_t S = std::tuple_size<head>::value;
+	typedef std::array<arraytype, S * (sizeof...(Args) + 1)> bigarray;
+
+
+	static void cat(bigarray& arr, head a, Args... tail){
+		cpy<0, S, bigarray, Args...>(arr, a, tail...);
+	}
+};
+
+template<typename... Args>
+typename concantinator<Args...>::bigarray
+cat(Args... tail){
+	typedef typename concantinator<Args...>::bigarray bigarr;
+	bigarr out;
+	concantinator<Args...>::cat(out, tail...);
+	return(out);
+}
+
+// int main() {
+// 	std::array<int, 3> a = { 1, 2, 3};
+// 	std::array<int, 3> b = { 1, 2, 3};
+// 	std::array<int, 3> c = { 5, 6, 8};
+// 	std::array<int, 9> d = cat(a, b, c);
+// 	for(int i = 0; i<9; i++)std::cout << d[i] << std::endl;
+// }
--- test/variadic/message_test.cpp
+++ test/variadic/message_test.cpp
@@ -15,6 +15,18 @@ TEST(message, compare)
 	EXPECT_EQ("My name is Gleb, Age = 21, and this is just percents %%%%%", ss.str());
 }
 
+TEST(cat, compare)
+{
+	std::array<int, 3> a = { 1, 2, 3};
+	std::array<int, 3> b = { 1, 2, 3};
+	std::array<int, 3> c = { 5, 6, 8};
+	std::array<int, 9> d = cat(a, b, c);
+	std::array<int, 9> q = {1, 2, 3, 1, 2, 3, 5, 6, 8};
+	for(int i=0; i<d.size(); i++) {
+		EXPECT_TRUE(d[i] == q[i]);
+	}
+}
+
 int main(int argc, char *argv[]) {
 	::testing::InitGoogleTest(&argc, argv);
 	return RUN_ALL_TESTS();
