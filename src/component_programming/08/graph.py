84af05cb8fec790ce0af5e0a2a9e14f55decd20e
glebasson <glebasson@yandex.ru>
2019-12-19 21:24:19 +0300

    prj structure refactored

--- src/component_programming/08/graph.py
+++ src/component_programming/08/graph.py
@@ -10,10 +10,17 @@ def sortwrapper(n):
     m, s = output.decode("utf-8").split()
     return m, s, float(int(s)/int(m))
 
-timestamps = [10, 100, 1000, 10000, 100000, 1000000, 10000000]
+timestamps = [10, 100, 1000, 10000, 100000, 1000000]
 
 data = [sortwrapper(n) for n in timestamps]
 
 df = pd.DataFrame(data, index=timestamps, columns=['multy','single', 'm/s'])
 print(df)
 
+# 4 threads
+# 10          1299321      155327  0.119545
+# 100         2210905     1683440  0.761426
+# 1000        7190045    10298496  1.432327
+# 10000      37916686    53352410  1.407096
+# 100000    215756707   602014712  2.790248
+# 1000000  2202604280  7234255324  3.284410
