87e217f7bdc149df886e9cd597514407b9a01e02
glebasson <glebasson@yandex.ru>
2019-12-24 06:19:57 +0300

    temp changes

---
+++ src/component_programming/10/rotate.cpp
@@ -0,0 +1,36 @@
+#include <utility>
+#include <stdio.h>
+#include <cassert>
+
+template<typename T> using P = std::pair<T, T>;
+template<typename T>
+P<T> rotate(const P<T>& vec, int degree) {
+    int sign = degree/std::abs(degree);
+    assert(degree % 90 == 0);
+    if (degree % 360 == 0) return P<T>(vec.first, vec.second);
+    if (degree % 180 == 0) return P<T>(-1 * vec.first, -1 * vec.second);
+    sign = degree % 90 ? sign * -1 : sign;
+    // граничные случаи
+    if (vec.first == 0 && (vec.second > 0 && sign < 0) || (vec.second < 0 && sign > 0) ) 
+        return P<T>(vec.second * sign * -1, 0);
+    if (vec.first == 0 && (vec.second > 0 && sign > 0) || (vec.second < 0 && sign < 0) ) 
+        return P<T>(vec.second * sign * -1, 0);
+    if (vec.second == 0 && (vec.first > 0 && sign < 0) || (vec.first < 0 && sign > 0) ) 
+        return P<T>(0, vec.first * sign);
+    if (vec.second == 0 && (vec.first > 0 && sign > 0) || (vec.first < 0 && sign < 0) ) 
+        return P<T>(0, vec.first * sign);
+    
+    if (sign > 0 && vec.first * vec.second > 0) { // 1 3 qurter
+        return P<T>(-1 * vec.second, vec.first);
+    }
+    if (sign > 0 && vec.first * vec.second < 0) { // 2 4 qurter
+        return P<T>(vec.second * -1, vec.first);
+    }
+    if (sign < 0 && vec.first * vec.second > 0) { // 1 3 qurter
+        return P<T>(-1 * vec.second, -1 * vec.first);
+    }
+    if (sign < 0 && vec.first * vec.second < 0) { // 2 4 qurter
+        return P<T>(vec.second, -1 * vec.first);
+    }
+}
+
---
+++ src/test/rotatetest/rotate_test.cpp
@@ -0,0 +1,12 @@
+#include <gtest/gtest.h>
+#include <10/rotate.cpp>
+
+TEST(cat, compare)
+{
+	// EXPECT_TRUE(d[i] == q[i]);
+}
+
+int main(int argc, char *argv[]) {
+	::testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
+}
